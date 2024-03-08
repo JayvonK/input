633c893e1cf26ffd1ad2a6eb260059fb9c702ee6
JayvonK <jaykhuth@gmail.com>
2024-03-08 09:57:01 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Col, Row } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col lg={12} md={9} sm={6} className='d-flex justify-content-center'>
+        <h1>Enter Name</h1>
+      </Col>
+
+      <Col lg={12} className='d-flex justify-content-center'>
+        <input />
+        <Button variant="dark">Save Name</Button>
+      </Col>
+    </ Row>
   );
 }
 
